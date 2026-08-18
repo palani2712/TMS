11d89acb0d26dbe25745b1421c23a19c012ce273
palani2712 <palaniappan2712@gmail.com>
2026-08-18 14:39:16 +0530

    commit

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -724,6 +724,7 @@ const Users = () => {
             </form>
           </div>
         </div>
+      )}
       {/* DELETE CONFIRMATION MODAL */}
       {isDeleteModalOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
